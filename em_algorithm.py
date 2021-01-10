2ad46e3ff26de984ba356115eaca9e6e5c6e47be
Roi Peretz <roip890@gmail.com>
2021-01-10 19:02:19 +0200

    matrices migration

--- em_algorithm.py
+++ em_algorithm.py
@@ -10,22 +10,18 @@ class ExpectationMaximizationAlgorithm(object):
         # algorithm params
         self.data = data
         print('start', time.time() - self.data.start_time)
-        print('expectation - start', time.time() - self.data.start_time)
-        self.expectation()
-        print('expectation - end', time.time() - self.data.start_time)
-        print('maximization - start', time.time() - self.data.start_time)
-        self.maximization()
-        print('maximization - end', time.time() - self.data.start_time)
-        print('expectation - start', time.time() - self.data.start_time)
-        self.expectation()
-        print('expectation - end', time.time() - self.data.start_time)
-        print('maximization - start', time.time() - self.data.start_time)
-        self.maximization()
-        print('maximization - end', time.time() - self.data.start_time)
+        for i in range(0, 20):
+            print('expectation - start', time.time() - self.data.start_time)
+            self.expectation()
+            print('expectation - end', time.time() - self.data.start_time)
+            print('maximization - start', time.time() - self.data.start_time)
+            self.maximization()
+            print('maximization - end', time.time() - self.data.start_time)
 
     # expectation step
     def expectation(self):
         self.normalize_alpha()
+        likelihood = 0
         for t in range(0, len(self.data.documents)):
             # update z_i for this document
             self.data.z[t] = np.array([self.calculate_z_i(t, j) for j in range(len(self.data.clusters))])
@@ -34,12 +30,15 @@ class ExpectationMaximizationAlgorithm(object):
             w_t_i_denominator = sum([math.pow(math.e, z_i) for z_i in z_i_minus_m if z_i > - self.data.k])
             for i in range(0, len(self.data.clusters)):
                 self.data.w[t][i] = 0 if z_i_minus_m[i] < - self.data.k else math.pow(math.e, z_i_minus_m[i]) / w_t_i_denominator
+            likelihood += self.data.z_m[t] + math.log(w_t_i_denominator)
+        print(likelihood)
+        self.data.likelihood = likelihood
 
     # maximization step
     def maximization(self):
         n = len(self.data.documents)
         lamb = self.data.lamb
-        lamb_v = self.data.lamb * sum(self.data.v)
+        lamb_v = self.data.lamb * len(self.data.v)
         for i in range(0, len(self.data.clusters)):
             self.data.a[i] = self.calculate_alpha_i(i, n)
             p_numerator = self.data.w.T.dot(self.data.n) + lamb
